8c852b6acd5de522ee7a30e6cf8347e93892c418
Naz <nazzzarr@gmail.com>
2024-02-28 16:28:40 +0200

    MASTER - CLIENT - implement typescript

---
+++ client/src/test.ts
@@ -0,0 +1,4 @@
+export function someFn(arg: number): string {
+	console.log('💛 ', `'Work is fine' ${arg}`);
+	return 'Work is fine' + arg;
+}
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,11 +1,35 @@
 const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const MODE = 'development';
+// const MODE = 'production';
 
 module.exports = {
-	mode: 'development',
-	entry: path.resolve(__dirname, 'src', 'index.js'),
+	mode: MODE,
+	entry: path.resolve(__dirname, 'src', 'index.ts'),
 	output: {
 		filename: '[name].[contenthash].js',
 		path: path.resolve(__dirname, 'build'),
 		clean: true,
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, 'public', 'index.html'),
+		}),
+		new webpack.ProgressPlugin(),
+	],
+	module: {
+		//Указываем расширения которые выходят за рамки
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/,
+			},
+		],
+	},
+	resolve: {
+		extensions: ['.tsx', '.ts', '.js'],
 	},
 };
