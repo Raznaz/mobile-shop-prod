b5b9524f08d16d7d3caa0a83b3f0f3d5ce381d4a
Naz <nazzzarr@gmail.com>
2024-03-08 08:11:20 +0200

    MASTER - CLIENT - refactoring loaders webpack config

--- client/config/build/buildLoaders.ts
+++ client/config/build/buildLoaders.ts
@@ -1,6 +1,7 @@
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import webpack from 'webpack';
 import { BuildOptions } from './types/config';
+import { buildCssLoaders } from './loaders/buildCssLoaders';
 
 export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
 	const { isDev } = options;
@@ -25,24 +26,7 @@ export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
 		},
 	};
 
-	const cssLoader = {
-		test: /\.s[ac]ss$/i,
-		use: [
-			options.isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-			{
-				loader: 'css-loader',
-				options: {
-					modules: {
-						auto: (resPath: string) => Boolean(resPath.includes('.module.')),
-						localIdentName: isDev
-							? '[path][name]__[local]--[hash:base64:5]'
-							: '[hash: base64:8]',
-					},
-				},
-			},
-			'sass-loader',
-		],
-	};
+	const cssLoader = buildCssLoaders(options.isDev);
 
 	const typeScriptLoader = {
 		test: /\.tsx?$/,
--- client/config/storybook/main.ts
+++ client/config/storybook/main.ts
@@ -1,7 +1,7 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import path from 'path';
 import { BuildPaths } from '../build/types/config';
+import { buildCssLoaders } from '../build/loaders/buildCssLoaders';
 
 const config: StorybookConfig = {
 	stories: ['../../src/**/*.mdx', '../../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
@@ -30,24 +30,7 @@ const config: StorybookConfig = {
 			src: path.resolve(__dirname, '..', '..', 'src'),
 		};
 
-		config.module?.rules?.push({
-			test: /\.s[ac]ss$/i,
-			use: [
-				true ? 'style-loader' : MiniCssExtractPlugin.loader,
-				{
-					loader: 'css-loader',
-					options: {
-						modules: {
-							auto: (resPath: string) => Boolean(resPath.includes('.module.')),
-							localIdentName: true
-								? '[path][name]__[local]--[hash:base64:5]'
-								: '[hash: base64:8]',
-						},
-					},
-				},
-				'sass-loader',
-			],
-		});
+		config.module?.rules?.push(buildCssLoaders(true));
 		config.resolve?.extensions?.push('ts', 'tsx');
 		config.resolve?.modules?.push(paths.src);
 
