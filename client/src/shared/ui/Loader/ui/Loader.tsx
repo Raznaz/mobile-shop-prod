aee8fe992b3a3b7d28e2ced8fb262f8d2e04a256
Naz <nazzzarr@gmail.com>
2024-03-08 19:13:16 +0200

    FEATURE - created storybook for Loader

--- client/src/shared/ui/Loader/ui/Loader.tsx
+++ client/src/shared/ui/Loader/ui/Loader.tsx
@@ -1,21 +1,21 @@
-import { classNames } from 'shared/lib/classNames/classNames';
+import React from 'react';
 import './Loader.scss';
 
 export const Loader = () => {
 	return (
 		<div className='lds-spinner'>
-			<div></div>
-			<div></div>
-			<div></div>
-			<div></div>
-			<div></div>
-			<div></div>
-			<div></div>
-			<div></div>
-			<div></div>
-			<div></div>
-			<div></div>
-			<div></div>
+			<div />
+			<div />
+			<div />
+			<div />
+			<div />
+			<div />
+			<div />
+			<div />
+			<div />
+			<div />
+			<div />
+			<div />
 		</div>
 	);
 };
