f58990b89af9432ef9d2691d581d1c4427a96d45
Naz <nazzzarr@gmail.com>
2024-03-18 16:05:26 +0200

    MASTER - CLIENT - refactor filter using useCallback

--- client/src/pages/PhoneBook/ui/PhoneBook.tsx
+++ client/src/pages/PhoneBook/ui/PhoneBook.tsx
@@ -1,61 +1,36 @@
 import axios from 'axios';
 import cls from './PhoneBook.module.scss';
 import { classNames } from 'shared/lib/classNames/classNames';
-import { useEffect, useState, ChangeEvent } from 'react';
+import { useEffect, useState, ChangeEvent, useRef, useCallback, useMemo } from 'react';
 import { fetchPhones } from 'app/provider/api/api';
 
 interface PhoneBookProps {
 	className?: string;
 }
 
-interface IPhoneBook {
-	firstName: string;
-	lastName: string;
-}
-
-type InputEvent = ChangeEvent<HTMLInputElement>;
+const API_URL = 'http://localhost:5000/api/phones';
 
 export const PhoneBook = ({ className }: PhoneBookProps) => {
 	const [phones, setPhones] = useState([]);
-	const [filteredPhones, setFilteredPhones] = useState([]);
+	const [query, setQuery] = useState('');
 
-	const API_URL = 'http://localhost:5000/api/phones';
+	const filteredPhones = useMemo(() => {
+		return phones.filter((phone) => {
+			return (
+				phone.firstName.toLowerCase().includes(query.toLowerCase()) ||
+				phone.lastName.toLowerCase().includes(query.toLowerCase())
+			);
+		});
+	}, [phones, query]);
 
 	const fetchPhoneBookDate = async () => {
 		try {
 			const { data } = await fetchPhones();
 
-			const myPhones: IPhoneBook = data;
-
 			setPhones(data);
-			setFilteredPhones(data);
 		} catch (error) {
 			throw new Error(error.message);
 		}
-	};
-
-	// const filterList = async (event: any) => {
-	// const { data } = await axios.get(`${API_URL}?query=${event.target.value}`);
-	// };
-
-	// const sortList = () => {
-	// return {};
-	// };
-
-	const filterList = async (event: InputEvent) => {
-		console.log('🟡 event', event.target.value);
-		const filteredData = phones.filter((phone) => {
-			if (
-				phone.firstName.toLowerCase().includes(event.target.value.toLocaleLowerCase()) ||
-				phone.lastName.toLowerCase().includes(event.target.value.toLocaleLowerCase())
-			) {
-				return phone;
-			}
-			return false;
-		});
-
-		console.log('🟢filteredData ', filteredData);
-		setFilteredPhones(filteredData);
 	};
 
 	useEffect(() => {
@@ -72,8 +47,9 @@ export const PhoneBook = ({ className }: PhoneBookProps) => {
 						<div>
 							<input
 								className={cls.search}
+								value={query}
 								type='search'
-								onChange={filterList}
+								onChange={(e) => setQuery(e.target.value)}
 								placeholder='Search...'
 							/>
 						</div>
